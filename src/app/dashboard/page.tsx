dce307c23391f95856f9fcaeb87bfac7ef677dcf
changwoolim <limcw@zarathu.com>
2024-05-01 22:59:26 +0900

    240501

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -6,10 +6,11 @@ import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
-
+import MyProjectList from "@/components/modules/dashboard/MyProjectList";
 export default function Dashboard() {
 	return (
 		<div className="flex min-h-screen w-full flex-col">
+			<MyProjectList />
 			<main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
 				<div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-4">
 					<Card x-chunk="dashboard-01-chunk-0">
--- src/components/global/Navbar.tsx
+++ src/components/global/Navbar.tsx
@@ -7,13 +7,13 @@ import Link from "next/link";
 export default function Navbar() {
 	return (
 		<header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
-			<nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
+			<nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6 whitespace-nowrap">
 				<Link href="#" className="flex items-center gap-2 text-lg font-semibold md:text-base">
 					<Package2 className="h-6 w-6" />
 					<span className="sr-only">Acme Inc</span>
 				</Link>
-				<Link href="#" className="text-foreground transition-colors hover:text-foreground">
-					Dashboard
+				<Link href="/dashboard" className="text-foreground transition-colors hover:text-foreground">
+					대시보드
 				</Link>
 				<Link href="#" className="text-muted-foreground transition-colors hover:text-foreground">
 					Orders
@@ -32,7 +32,7 @@ export default function Navbar() {
 				<SheetTrigger asChild>
 					<Button variant="outline" size="icon" className="shrink-0 md:hidden">
 						<Menu className="h-5 w-5" />
-						<span className="sr-only">Toggle navigation menu</span>
+						<span className="sr-only">메뉴 열기</span>
 					</Button>
 				</SheetTrigger>
 				<SheetContent side="left">
@@ -42,7 +42,7 @@ export default function Navbar() {
 							<span className="sr-only">Acme Inc</span>
 						</Link>
 						<Link href="#" className="hover:text-foreground">
-							Dashboard
+							대시보드
 						</Link>
 						<Link href="#" className="text-muted-foreground hover:text-foreground">
 							Orders
@@ -63,23 +63,23 @@ export default function Navbar() {
 				<form className="ml-auto flex-1 sm:flex-initial">
 					<div className="relative">
 						<Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-						<Input type="search" placeholder="Search products..." className="pl-8 sm:w-[300px] md:w-[200px] lg:w-[300px]" />
+						<Input type="search" placeholder="연구노트 검색" className="pl-8 sm:w-[300px] md:w-[200px] lg:w-[300px]" />
 					</div>
 				</form>
 				<DropdownMenu>
 					<DropdownMenuTrigger asChild>
 						<Button variant="secondary" size="icon" className="rounded-full">
 							<CircleUser className="h-5 w-5" />
-							<span className="sr-only">Toggle user menu</span>
+							<span className="sr-only">유저 메뉴 열기</span>
 						</Button>
 					</DropdownMenuTrigger>
 					<DropdownMenuContent align="end">
-						<DropdownMenuLabel>My Account</DropdownMenuLabel>
+						<DropdownMenuLabel>내 계정</DropdownMenuLabel>
 						<DropdownMenuSeparator />
-						<DropdownMenuItem>Settings</DropdownMenuItem>
-						<DropdownMenuItem>Support</DropdownMenuItem>
+						<DropdownMenuItem>설정</DropdownMenuItem>
+						<DropdownMenuItem>고객지원</DropdownMenuItem>
 						<DropdownMenuSeparator />
-						<DropdownMenuItem>Logout</DropdownMenuItem>
+						<DropdownMenuItem>로그아웃</DropdownMenuItem>
 					</DropdownMenuContent>
 				</DropdownMenu>
 			</div>
