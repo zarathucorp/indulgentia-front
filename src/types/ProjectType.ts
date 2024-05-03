11dd34c90fa8e8960e43a96d39348793e665fa37
changwoolim <limcw@zarathu.com>
2024-05-04 02:41:09 +0900

    Add Bucket, Project Type

---
+++ src/types/ProjectType.ts
@@ -0,0 +1,9 @@
+export default interface ProjectType {
+	project_name: string;
+	project_status: string;
+	project_leader: string;
+	project_members: number;
+	project_peroid_start: Date;
+	project_peroid_end: Date;
+	project_uuid: string;
+}
