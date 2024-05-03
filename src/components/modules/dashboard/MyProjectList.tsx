106fca66cbee2bba5c88531dd18c92fe0350469d
changwoolim <limcw@zarathu.com>
2024-05-04 02:41:40 +0900

    Add Project, Bucket Component

--- src/components/modules/dashboard/MyProjectList.tsx
+++ src/components/modules/dashboard/MyProjectList.tsx
@@ -7,7 +7,8 @@ import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle }
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import Link from "next/link";
-export default function MyProjectList() {
+import ProjectType from "@/types/ProjectType";
+export default function MyProjectList({ projectList }: { projectList: ProjectType[] }) {
 	return (
 		<Card>
 			<CardHeader>
@@ -19,14 +20,12 @@ export default function MyProjectList() {
 				<Table>
 					<TableHeader>
 						<TableRow>
-							<TableHead className="hidden w-[100px] sm:table-cell">
-								<span className="sr-only">이미지</span>
-							</TableHead>
 							<TableHead>프로젝트 이름</TableHead>
 							<TableHead>상태</TableHead>
 							<TableHead className="hidden md:table-cell">연구책임자</TableHead>
 							<TableHead className="hidden md:table-cell">참여 연구원 수</TableHead>
-							<TableHead className="hidden md:table-cell">수행 기간</TableHead>
+							<TableHead className="hidden md:table-cell">수행 시작일</TableHead>
+							<TableHead className="hidden md:table-cell">수행 종료일</TableHead>
 							<TableHead>
 								<span className="sr-only">Actions</span>
 							</TableHead>
@@ -34,176 +33,34 @@ export default function MyProjectList() {
 					</TableHeader>
 					<TableBody>
 						<TableRow>
-							<TableCell className="hidden sm:table-cell">
-								<Image alt="Product image" className="aspect-square rounded-md object-cover" height="64" src="/placeholder.svg" width="64" />
-							</TableCell>
-							<TableCell className="font-medium">Laser Lemonade Machine</TableCell>
-							<TableCell>
-								<Badge variant="outline">Draft</Badge>
-							</TableCell>
-							<TableCell className="hidden md:table-cell">$499.99</TableCell>
-							<TableCell className="hidden md:table-cell">25</TableCell>
-							<TableCell className="hidden md:table-cell">2023-07-12 10:42 AM</TableCell>
-							<TableCell>
-								<Button>
-									<Link href="/dashboard/bucket/uuid">관리</Link>
-								</Button>
-								{/* <DropdownMenu>
-									<DropdownMenuTrigger asChild>
-										<Button aria-haspopup="true" size="icon" variant="ghost">
-											<MoreHorizontal className="h-4 w-4" />
-											<span className="sr-only">Toggle menu</span>
-										</Button>
-									</DropdownMenuTrigger>
-									<DropdownMenuContent align="end">
-										<DropdownMenuLabel>Actions</DropdownMenuLabel>
-										<DropdownMenuItem>Edit</DropdownMenuItem>
-										<DropdownMenuItem>Delete</DropdownMenuItem>
-									</DropdownMenuContent>
-								</DropdownMenu> */}
-							</TableCell>
-						</TableRow>
-						<TableRow>
-							<TableCell className="hidden sm:table-cell">
-								<Image alt="Product image" className="aspect-square rounded-md object-cover" height="64" src="/placeholder.svg" width="64" />
-							</TableCell>
-							<TableCell className="font-medium">Hypernova Headphones</TableCell>
-							<TableCell>
-								<Badge variant="outline">Active</Badge>
-							</TableCell>
-							<TableCell className="hidden md:table-cell">$129.99</TableCell>
-							<TableCell className="hidden md:table-cell">100</TableCell>
-							<TableCell className="hidden md:table-cell">2023-10-18 03:21 PM</TableCell>
-							<TableCell>
-								<DropdownMenu>
-									<DropdownMenuTrigger asChild>
-										<Button aria-haspopup="true" size="icon" variant="ghost">
-											<MoreHorizontal className="h-4 w-4" />
-											<span className="sr-only">Toggle menu</span>
-										</Button>
-									</DropdownMenuTrigger>
-									<DropdownMenuContent align="end">
-										<DropdownMenuLabel>Actions</DropdownMenuLabel>
-										<DropdownMenuItem>Edit</DropdownMenuItem>
-										<DropdownMenuItem>Delete</DropdownMenuItem>
-									</DropdownMenuContent>
-								</DropdownMenu>
-							</TableCell>
-						</TableRow>
-						<TableRow>
-							<TableCell className="hidden sm:table-cell">
-								<Image alt="Product image" className="aspect-square rounded-md object-cover" height="64" src="/placeholder.svg" width="64" />
-							</TableCell>
-							<TableCell className="font-medium">AeroGlow Desk Lamp</TableCell>
-							<TableCell>
-								<Badge variant="outline">Active</Badge>
-							</TableCell>
-							<TableCell className="hidden md:table-cell">$39.99</TableCell>
-							<TableCell className="hidden md:table-cell">50</TableCell>
-							<TableCell className="hidden md:table-cell">2023-11-29 08:15 AM</TableCell>
-							<TableCell>
-								<DropdownMenu>
-									<DropdownMenuTrigger asChild>
-										<Button aria-haspopup="true" size="icon" variant="ghost">
-											<MoreHorizontal className="h-4 w-4" />
-											<span className="sr-only">Toggle menu</span>
-										</Button>
-									</DropdownMenuTrigger>
-									<DropdownMenuContent align="end">
-										<DropdownMenuLabel>Actions</DropdownMenuLabel>
-										<DropdownMenuItem>Edit</DropdownMenuItem>
-										<DropdownMenuItem>Delete</DropdownMenuItem>
-									</DropdownMenuContent>
-								</DropdownMenu>
-							</TableCell>
-						</TableRow>
-						<TableRow>
-							<TableCell className="hidden sm:table-cell">
-								<Image alt="Product image" className="aspect-square rounded-md object-cover" height="64" src="/placeholder.svg" width="64" />
-							</TableCell>
-							<TableCell className="font-medium">TechTonic Energy Drink</TableCell>
-							<TableCell>
-								<Badge variant="secondary">Draft</Badge>
-							</TableCell>
-							<TableCell className="hidden md:table-cell">$2.99</TableCell>
-							<TableCell className="hidden md:table-cell">0</TableCell>
-							<TableCell className="hidden md:table-cell">2023-12-25 11:59 PM</TableCell>
-							<TableCell>
-								<DropdownMenu>
-									<DropdownMenuTrigger asChild>
-										<Button aria-haspopup="true" size="icon" variant="ghost">
-											<MoreHorizontal className="h-4 w-4" />
-											<span className="sr-only">Toggle menu</span>
-										</Button>
-									</DropdownMenuTrigger>
-									<DropdownMenuContent align="end">
-										<DropdownMenuLabel>Actions</DropdownMenuLabel>
-										<DropdownMenuItem>Edit</DropdownMenuItem>
-										<DropdownMenuItem>Delete</DropdownMenuItem>
-									</DropdownMenuContent>
-								</DropdownMenu>
-							</TableCell>
-						</TableRow>
-						<TableRow>
-							<TableCell className="hidden sm:table-cell">
-								<Image alt="Product image" className="aspect-square rounded-md object-cover" height="64" src="/placeholder.svg" width="64" />
-							</TableCell>
-							<TableCell className="font-medium">Gamer Gear Pro Controller</TableCell>
-							<TableCell>
-								<Badge variant="outline">Active</Badge>
-							</TableCell>
-							<TableCell className="hidden md:table-cell">$59.99</TableCell>
-							<TableCell className="hidden md:table-cell">75</TableCell>
-							<TableCell className="hidden md:table-cell">2024-01-01 12:00 AM</TableCell>
-							<TableCell>
-								<DropdownMenu>
-									<DropdownMenuTrigger asChild>
-										<Button aria-haspopup="true" size="icon" variant="ghost">
-											<MoreHorizontal className="h-4 w-4" />
-											<span className="sr-only">Toggle menu</span>
-										</Button>
-									</DropdownMenuTrigger>
-									<DropdownMenuContent align="end">
-										<DropdownMenuLabel>Actions</DropdownMenuLabel>
-										<DropdownMenuItem>Edit</DropdownMenuItem>
-										<DropdownMenuItem>Delete</DropdownMenuItem>
-									</DropdownMenuContent>
-								</DropdownMenu>
-							</TableCell>
-						</TableRow>
-						<TableRow>
-							<TableCell className="hidden sm:table-cell">
-								<Image alt="Product image" className="aspect-square rounded-md object-cover" height="64" src="/placeholder.svg" width="64" />
-							</TableCell>
-							<TableCell className="font-medium">Luminous VR Headset</TableCell>
-							<TableCell>
-								<Badge variant="outline">Active</Badge>
-							</TableCell>
-							<TableCell className="hidden md:table-cell">$199.99</TableCell>
-							<TableCell className="hidden md:table-cell">30</TableCell>
-							<TableCell className="hidden md:table-cell">2024-02-14 02:14 PM</TableCell>
-							<TableCell>
-								<DropdownMenu>
-									<DropdownMenuTrigger asChild>
-										<Button aria-haspopup="true" size="icon" variant="ghost">
-											<MoreHorizontal className="h-4 w-4" />
-											<span className="sr-only">Toggle menu</span>
-										</Button>
-									</DropdownMenuTrigger>
-									<DropdownMenuContent align="end">
-										<DropdownMenuLabel>Actions</DropdownMenuLabel>
-										<DropdownMenuItem>Edit</DropdownMenuItem>
-										<DropdownMenuItem>Delete</DropdownMenuItem>
-									</DropdownMenuContent>
-								</DropdownMenu>
-							</TableCell>
+							{projectList.map((project: ProjectType) => {
+								return (
+									<>
+										<TableCell className="font-medium">{project.project_name}</TableCell>
+										<TableCell>
+											<Badge variant="outline">{project.project_status}</Badge>
+										</TableCell>
+										<TableCell className="hidden md:table-cell">{project.project_leader}</TableCell>
+										<TableCell className="hidden md:table-cell">{project.project_members}</TableCell>
+										<TableCell className="hidden md:table-cell">{project.project_peroid_start.toDateString()}</TableCell>
+										<TableCell className="hidden md:table-cell">{project.project_peroid_end.toDateString()}</TableCell>
+										<TableCell>
+											<Button>
+												<Link href={`/dashboard/project/${project.project_uuid}`}>관리</Link>
+											</Button>
+										</TableCell>
+									</>
+								);
+							})}
 						</TableRow>
 					</TableBody>
 				</Table>
 			</CardContent>
 			<CardFooter>
 				<div className="text-xs text-muted-foreground">
-					Showing <strong>1-10</strong> of <strong>32</strong> products
+					Showing&nbsp;
+					{/* <strong>1-10</strong> of */}
+					<strong>{projectList.length}</strong> product{projectList.length > 1 ? "s" : ""}
 				</div>
 			</CardFooter>
 		</Card>
