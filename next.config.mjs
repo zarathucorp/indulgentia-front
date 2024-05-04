5406300ea01ece1140126dd08cd740ede9294087
changwoolim <limcw@zarathu.com>
2024-05-04 11:30:12 +0900

    Add PDFViewer

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	// react-pdf 지원
+	// webpack: (config) => {
+	// 	config.resolve.alias.canvas = false;
+	// 	return config;
+	// },
+};
 
 export default nextConfig;
---
+++ src/components/global/PDFViewer.tsx
@@ -0,0 +1,24 @@
+"use client";
+import { Worker, Viewer } from "@react-pdf-viewer/core";
+import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
+import "@react-pdf-viewer/core/lib/styles/index.css";
+import "@react-pdf-viewer/default-layout/lib/styles/index.css";
+
+interface PDFViewerProps {
+	fileUrl: string;
+	className?: any;
+}
+
+const PDFViewer: React.FC<PDFViewerProps> = ({ fileUrl, className }) => {
+	const defaultLayoutPluginInstance = defaultLayoutPlugin();
+
+	return (
+		<div style={{ height: "750px" }} className={className}>
+			<Worker workerUrl={`https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js`}>
+				<Viewer fileUrl={fileUrl} plugins={[defaultLayoutPluginInstance]} />
+			</Worker>
+		</div>
+	);
+};
+
+export default PDFViewer;
