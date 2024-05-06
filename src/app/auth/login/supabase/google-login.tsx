05439d59a8bda320c5372bda8243adc7b917e1fd
changwoolim <limcw@zarathu.com>
2024-05-06 11:17:00 +0900

    Add: Google Login

---
+++ src/app/auth/login/supabase/google-login.tsx
@@ -0,0 +1,28 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { FaGoogle } from "react-icons/fa";
+import { createClient } from "@/utils/supabase/client";
+const googleSignIn = async () => {
+	// "use server";
+	const supabase = createClient();
+	const { data, error } = await supabase.auth.signInWithOAuth({
+		provider: "google",
+		options: {
+			// queryParams: {
+			// 	access_type: "offline",
+			// 	prompt: "consent",
+			// },
+			redirectTo: `${location.origin}/auth/callback`,
+		},
+	});
+	console.log("data: \n", data, "error: \n", error);
+};
+
+export default function GoogleLoginButton() {
+	return (
+		<Button onClick={googleSignIn} className="w-full">
+			<FaGoogle />
+			&nbsp; Login with Google
+		</Button>
+	);
+}
--- src/app/auth/login/supabase/page.tsx
+++ src/app/auth/login/supabase/page.tsx
@@ -3,7 +3,7 @@ import { headers } from "next/headers";
 import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
 import SubmitButton from "./submit-button";
-import AuthButton from "./AuthButton";
+import GoogleLoginButton from "./google-login";
 export default function Login({ searchParams }: { searchParams: { message: string } }) {
 	const signIn = async (formData: FormData) => {
 		"use server";
@@ -49,25 +49,6 @@ export default function Login({ searchParams }: { searchParams: { message: strin
 
 	return (
 		<div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
-			<AuthButton />
-			<Link href="/" className="absolute left-8 top-8 py-2 px-4 rounded-md no-underline text-foreground bg-btn-background hover:bg-btn-background-hover flex items-center group text-sm">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					width="24"
-					height="24"
-					viewBox="0 0 24 24"
-					fill="none"
-					stroke="currentColor"
-					strokeWidth="2"
-					strokeLinecap="round"
-					strokeLinejoin="round"
-					className="mr-2 h-4 w-4 transition-transform group-hover:-translate-x-1"
-				>
-					<polyline points="15 18 9 12 15 6" />
-				</svg>{" "}
-				Back
-			</Link>
-
 			<form className="animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground">
 				<label className="text-md" htmlFor="email">
 					Email
@@ -85,6 +66,7 @@ export default function Login({ searchParams }: { searchParams: { message: strin
 				</SubmitButton>
 				{searchParams?.message && <p className="mt-4 p-4 bg-foreground/10 text-foreground text-center">{searchParams.message}</p>}
 			</form>
+			<GoogleLoginButton />
 		</div>
 	);
 }
