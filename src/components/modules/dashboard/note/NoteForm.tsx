375840a67d3f558d735734867a37034f4649366f
changwoolim <limcw@zarathu.com>
2024-05-21 13:30:16 +0900

    Feature: File upload

---
+++ src/components/modules/dashboard/note/NoteForm.tsx
@@ -0,0 +1,212 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import axios from "axios";
+import { Button } from "@/components/ui/button";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { useEffect, useState } from "react";
+import { createClient } from "@/utils/supabase/client";
+import { useSearchParams } from "next/navigation";
+
+const NoteSchema = z.object({
+	title: z
+		.string()
+		.min(1, {
+			message: "Bucket 이름은 1자보다 길어야 합니다.",
+		})
+		.max(1000, {
+			message: "Bucket 이름은 1,000자보다 짧아야 합니다.",
+		}),
+	description: z.string().min(1, { message: "노트 내용은 1자보다 길어야 합니다." }),
+	tags: z.string().optional(),
+	files: z.array(z.instanceof(File)).optional(),
+	bucket_id: z.string().uuid(),
+});
+
+export type CreateNoteFormValues = z.infer<typeof NoteSchema>;
+
+export default function NewNoteForm() {
+	const [userUUID, setUserUUID] = useState<string | null>(null);
+
+	useEffect(() => {
+		const supabase = createClient();
+		const getUserUUID = async () => {
+			const {
+				data: { user },
+			} = await supabase.auth.getUser();
+			if (user) {
+				setUserUUID(user.id);
+			} else {
+				console.log("No user is logged in");
+			}
+		};
+		getUserUUID();
+	}, []);
+
+	const searchParams = useSearchParams();
+	const bucketUUID: string = searchParams.get("bucket") as string;
+
+	const defaultValues: Partial<CreateNoteFormValues> = {
+		bucket_id: bucketUUID,
+	};
+
+	const form = useForm<CreateNoteFormValues>({
+		resolver: zodResolver(NoteSchema),
+		defaultValues,
+	});
+
+	async function onSubmit(data: CreateNoteFormValues) {
+		const validationResult = NoteSchema.safeParse(data);
+		if (!validationResult.success) {
+			console.error(validationResult.error.errors);
+			return;
+		}
+
+		const sendData = new FormData();
+		if (data.files) {
+			data.files.forEach((file) => {
+				console.log(file);
+				sendData.append("files", file);
+			});
+		}
+
+		sendData.append("title", data.title);
+		sendData.append("file_name", data.title);
+		sendData.append("bucket_id", data.bucket_id);
+		sendData.append("is_github", false.toString());
+
+		try {
+			const result = await axios.post(process.env.NEXT_PUBLIC_API_URL + "/dashboard/note/", sendData, {
+				withCredentials: true,
+				headers: {
+					"Content-Type": "multipart/form-data",
+				},
+			});
+			console.log(result);
+		} catch (error) {
+			console.error(error);
+		}
+	}
+
+	return (
+		<Form {...form}>
+			<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+				<NoteTitleField form={form} />
+				<NoteDescriptionField form={form} />
+				<NoteTagField form={form} />
+				<NoteFileField form={form} />
+				<BucketUUIDField form={form} />
+				<Button type="submit">새 Note 생성</Button>
+			</form>
+		</Form>
+	);
+}
+
+function NoteTitleField({ form }: { form: any }) {
+	return (
+		<FormField
+			control={form.control}
+			name="title"
+			render={({ field }) => (
+				<FormItem>
+					<FormLabel>노트 이름</FormLabel>
+					<FormControl>
+						<Input placeholder="노트 이름" {...field} />
+					</FormControl>
+					<FormDescription>노트 이름을 입력합니다.</FormDescription>
+					<FormMessage />
+				</FormItem>
+			)}
+		/>
+	);
+}
+
+function NoteDescriptionField({ form }: { form: any }) {
+	return (
+		<FormField
+			control={form.control}
+			name="description"
+			render={({ field }) => (
+				<FormItem>
+					<FormLabel>노트 내용</FormLabel>
+					<FormControl>
+						<Textarea placeholder="노트 내용을 입력하여 주십시오." {...field} />
+					</FormControl>
+					<FormDescription>노트 내용을 입력합니다.</FormDescription>
+					<FormMessage />
+				</FormItem>
+			)}
+		/>
+	);
+}
+
+function NoteTagField({ form }: { form: any }) {
+	return (
+		<FormField
+			control={form.control}
+			name="tags"
+			render={({ field }) => (
+				<FormItem>
+					<FormLabel>노트 태그</FormLabel>
+					<FormControl>
+						<Input placeholder="노트 태그" {...field} />
+					</FormControl>
+					<FormDescription>&quot;,&quot;로 구분된 태그를 입력합니다.</FormDescription>
+					<FormMessage />
+				</FormItem>
+			)}
+		/>
+	);
+}
+
+function NoteFileField({ form }: { form: any }) {
+	return (
+		<FormField
+			control={form.control}
+			name="files"
+			render={({ field }) => (
+				<FormItem>
+					<FormLabel>노트 파일</FormLabel>
+					<FormControl>
+						<input
+							type="file"
+							multiple
+							onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+								const files = event.target.files;
+								if (files) {
+									field.onChange(Array.from(files));
+								}
+							}}
+							placeholder="노트 파일"
+						/>
+					</FormControl>
+					<FormDescription>노트 파일을 업로드합니다.</FormDescription>
+					<FormMessage />
+				</FormItem>
+			)}
+		/>
+	);
+}
+
+function BucketUUIDField({ form }: { form: any }) {
+	return (
+		<FormField
+			control={form.control}
+			name="bucket_id"
+			render={({ field }) => (
+				<FormItem>
+					<FormLabel>Bucket UUID</FormLabel>
+					<FormControl>
+						<Input disabled {...field} />
+					</FormControl>
+					<FormDescription>Bucket UUID입니다.</FormDescription>
+					<FormMessage />
+				</FormItem>
+			)}
+		/>
+	);
+}
