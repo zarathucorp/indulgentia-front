8a82a7d6ae7c4ab142199c926cbe48c61ee54d07
Changwoo Lim <limcw@zarathu.com>
2024-05-24 12:29:04 +0000

    Add Home at Breadcrumb

--- src/components/modules/dashboard/DashboardBreadCrumb.tsx
+++ src/components/modules/dashboard/DashboardBreadCrumb.tsx
@@ -1,5 +1,5 @@
 import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbSeparator } from "@/components/ui/breadcrumb";
-
+import { FaHome } from "react-icons/fa";
 type DashboardLevel = {
 	level: "Dashboard";
 };
@@ -30,14 +30,29 @@ type NoteLevel = {
 
 type DashboardBreadCrumbType = DashboardLevel | ProjectLevel | BucketLevel | NoteLevel;
 
+function isProjectLevel(data: DashboardBreadCrumbType): data is ProjectLevel {
+	return data.level === "Project";
+}
+
+function isBucketOrNoteLevel(data: DashboardBreadCrumbType): data is BucketLevel | NoteLevel {
+	return data.level === "Bucket" || data.level === "Note";
+}
+
+function isNoteLevel(data: DashboardBreadCrumbType): data is NoteLevel {
+	return data.level === "Note";
+}
+
 export function DashboardBreadCrumb({ breadcrumbData }: { breadcrumbData: DashboardBreadCrumbType }) {
 	return (
 		<Breadcrumb>
 			<BreadcrumbList>
+				<BreadcrumbLink href="/">
+					<FaHome />
+				</BreadcrumbLink>
 				<BreadcrumbSeparator />
 				{breadcrumbData.level === "Dashboard" ? <BreadcrumbItem>대시보드</BreadcrumbItem> : <BreadcrumbLink href="/dashboard">대시보드</BreadcrumbLink>}
-				{/* 프로젝트 */}
-				{(breadcrumbData.level === "Project" || breadcrumbData.level === "Bucket" || breadcrumbData.level === "Note") && (
+
+				{(isProjectLevel(breadcrumbData) || isBucketOrNoteLevel(breadcrumbData)) && (
 					<>
 						<BreadcrumbSeparator />
 						{breadcrumbData.level === "Project" ? (
@@ -45,24 +60,24 @@ export function DashboardBreadCrumb({ breadcrumbData }: { breadcrumbData: Dashbo
 						) : (
 							<BreadcrumbLink href={`/dashboard/project/${breadcrumbData.project_id}`}>{breadcrumbData.project_title}</BreadcrumbLink>
 						)}
-						{(breadcrumbData.level === "Bucket" || breadcrumbData.level === "Note") && (
-							<>
-								<BreadcrumbSeparator />
-								{/* 버킷 */}
-								{breadcrumbData.level === "Bucket" ? (
-									<BreadcrumbItem>{breadcrumbData.bucket_title}</BreadcrumbItem>
-								) : (
-									<BreadcrumbLink href={`/dashboard/bucket/${breadcrumbData.bucket_id}`}>{breadcrumbData.bucket_title}</BreadcrumbLink>
-								)}
-								{breadcrumbData.level === "Note" && (
-									<>
-										<BreadcrumbSeparator />
-										{/* 노트 */}
-										<BreadcrumbItem>{breadcrumbData.note_title}</BreadcrumbItem>
-									</>
-								)}
-							</>
+					</>
+				)}
+
+				{isBucketOrNoteLevel(breadcrumbData) && (
+					<>
+						<BreadcrumbSeparator />
+						{breadcrumbData.level === "Bucket" ? (
+							<BreadcrumbItem>{breadcrumbData.bucket_title}</BreadcrumbItem>
+						) : (
+							<BreadcrumbLink href={`/dashboard/bucket/${breadcrumbData.bucket_id}`}>{breadcrumbData.bucket_title}</BreadcrumbLink>
 						)}
+					</>
+				)}
+
+				{isNoteLevel(breadcrumbData) && (
+					<>
+						<BreadcrumbSeparator />
+						<BreadcrumbItem>{breadcrumbData.note_title}</BreadcrumbItem>
 					</>
 				)}
 			</BreadcrumbList>
