1453889d82d7caff9f4fd08b2e13036d443d8d66
Changwoo Lim <limcw@zarathu.com>
2024-05-27 04:56:09 +0000

    Add spinner when loading

---
+++ src/components/global/Spinner.tsx
@@ -0,0 +1,3 @@
+export const Spinner = ({ size = "1em" }) => {
+	return <div className="spinner border-2 border-t-2 border-t-primary border-gray-200 rounded-full animate-spin" style={{ width: size, height: size }}></div>;
+};
