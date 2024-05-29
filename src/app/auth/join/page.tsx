4cfb519d0e84b681a47a320c8b5637bec3939bd8
Changwoo Lim <limcw@zarathu.com>
2024-05-29 08:10:14 +0000

    Handle First/Last name when join

--- src/app/auth/join/page.tsx
+++ src/app/auth/join/page.tsx
@@ -16,12 +16,18 @@ export default function JoinForm() {
 		const origin = headers().get("origin");
 		const email = formData.get("email") as string;
 		const password = formData.get("password") as string;
+		const lastName = formData.get("last-name") as string;
+		const firstName = formData.get("first-name") as string;
 		// console.log(email, password);
 		const supabase = createClient();
 		const { data, error } = await supabase.auth.signUp({
 			email,
 			password,
 			options: {
+				data: {
+					first_name: firstName,
+					last_name: lastName,
+				},
 				emailRedirectTo: `${origin}/auth/callback`,
 			},
 		});
