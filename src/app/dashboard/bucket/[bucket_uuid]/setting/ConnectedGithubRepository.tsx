bcc8f58eddb64d4fdedcf7973745452cbfb8e862
Changwoo Lim <limcw@zarathu.com>
2024-05-29 08:09:52 +0000

    Add Disconnect Github Repository Modal

--- src/app/dashboard/bucket/[bucket_uuid]/setting/ConnectedGithubRepository.tsx
+++ src/app/dashboard/bucket/[bucket_uuid]/setting/ConnectedGithubRepository.tsx
@@ -2,13 +2,16 @@
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { UUID } from "crypto";
 import { useVariable } from "@/hooks/useVariable";
 import { KeyedMutator } from "swr";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
-
+import { useToast } from "@/components/ui/use-toast";
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import { ActionButton } from "@/components/ui/actionbutton";
 type GithubRepoType = {
 	id: UUID;
 	bucket_id: UUID;
@@ -17,6 +20,54 @@ type GithubRepoType = {
 	repo_url: string;
 	user_id: UUID;
 };
+
+export function RemoveRepositoryModal({ repo }: { repo: GithubRepoType }) {
+	const { toast } = useToast();
+	const [isOpen, setIsOpen] = useState(false); // State to manage modal open/close
+	const disconnectRepository = async () => {
+		try {
+			await axios.delete(process.env.NEXT_PUBLIC_API_URl + `/dashboard/bucket/gitrepo/${repo.id}`);
+			toast({
+				title: "Repository 연결 해제",
+				description: "Repository의 연결이 해제되었습니다.",
+			});
+			setIsOpen(false);
+		} catch (e) {
+			console.error(e);
+			toast({
+				title: "Repository 연결 해제 실패",
+				description: "Repository의 연결 해제에 실패하였습니다.",
+			});
+		}
+	};
+
+	return (
+		<Dialog open={isOpen} onOpenChange={setIsOpen}>
+			<DialogTrigger asChild>
+				<Button className="text-gray-500 hover:text-red-500 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2" size="icon" variant="ghost">
+					<TrashIcon className="w-5 h-5" />
+				</Button>
+			</DialogTrigger>
+			<DialogContent className="sm:max-w-[425px]">
+				<DialogHeader>
+					<DialogTitle>Repository 연결 해제</DialogTitle>
+					<DialogDescription>Repository의 연결을 해제합니다. 이미 생성된 노트는 삭제되지 않습니다. 언제든지 이 페이지에서 다시 연결할 수 있습니다.</DialogDescription>
+				</DialogHeader>
+				<div className="grid gap-4 py-4">
+					<div className="grid grid-cols-4 items-center gap-4">
+						<Label htmlFor="username" className="text-right">
+							삭제 대상
+						</Label>
+						<Label className="col-span-3">{repo.git_username + "/" + repo.git_repository} </Label>
+					</div>
+				</div>
+				<DialogFooter>
+					<ActionButton onClick={disconnectRepository}>삭제</ActionButton>
+				</DialogFooter>
+			</DialogContent>
+		</Dialog>
+	);
+}
 
 export default function ConnectedGithubRepository({ connectedGithubRepos }: { connectedGithubRepos: GithubRepoType[] }) {
 	return (
@@ -34,9 +85,7 @@ export default function ConnectedGithubRepository({ connectedGithubRepos }: { co
 										<h3 className="text-lg font-bold">{repo.git_repository}</h3>
 										<p className="text-gray-500">{repo.user_id}</p>
 									</div>
-									<Button className="text-gray-500 hover:text-red-500 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2" size="icon" variant="ghost">
-										<TrashIcon className="w-5 h-5" />
-									</Button>
+									<RemoveRepositoryModal repo={repo} />
 								</div>
 							</div>
 						</>
