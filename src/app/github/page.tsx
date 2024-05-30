6033b538d5def06f278d459def9b2e4ceee6509a
Changwoo Lim <limcw@zarathu.com>
2024-05-30 13:59:09 +0000

    Comment unused codes

--- src/app/github/page.tsx
+++ src/app/github/page.tsx
@@ -3,8 +3,10 @@ import Link from "next/link";
 export default function Home() {
 	return (
 		<div>
-			<h1>Welcome to GitHub App</h1>
+			{/* <h1>Welcome to GitHub App</h1>
 			<Link href="/next-api/github/login">Login with GitHub</Link>
+			<br />
+			<Link href={process.env.NEXT_PUBLIC_GITHUB_APP_INSTALL_URL || "#"}>Install GitHub App</Link> */}
 		</div>
 	);
 }
