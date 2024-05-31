780987d99546626a7658757df8704fbaeeab9154
Changwoo Lim <limcw@zarathu.com>
2024-05-31 02:11:11 +0000

    Update ChannelTalk to implement user info

--- src/components/global/ChannelTalk.tsx
+++ src/components/global/ChannelTalk.tsx
@@ -1,15 +1,28 @@
 "use client";
+import { createClient } from "@/utils/supabase/client";
 import * as ChannelService from "@channel.io/channel-web-sdk-loader";
-
+import { useEffect } from "react";
 ChannelService.loadScript();
 
-// 나중에 유저 정보 받아서 입력해줘야 함.
-// 참고: https://developers.channel.io/reference/glossary-kr#%EB%A9%A4%EB%B2%84-%EC%9C%A0%EC%A0%80
-// 참고: https://developers.channel.io/reference/web-quickstart-kr
-
 export default function ChannelTalk() {
-	ChannelService.boot({
-		pluginKey: process.env.NEXT_PUBLIC_CHANNEL_TALK_PLUGIN_KEY as string, // fill your plugin key
-	});
+	useEffect(() => {
+		const supabase = createClient();
+		const getCurrentUserKey = async () => {
+			const {
+				data: { user },
+			} = await supabase.auth.getUser();
+			ChannelService.boot({
+				pluginKey: process.env.NEXT_PUBLIC_CHANNEL_TALK_PLUGIN_KEY as string, // fill your plugin key
+				memberId: user?.id,
+				profile: {
+					name: user?.email || null, // 의도적으로 email로 넣었습니다 => 추후 수정 필요; 채널톡 유료플랜 안 쓰면 이메일 못 보는데, 이름만 들어갈 경우에 회원 식별이 불가합니다.
+					email: user?.email || null,
+					mobileNumber: user?.phone || null,
+				},
+			});
+		};
+		getCurrentUserKey();
+	}, []);
+
 	return null;
 }
