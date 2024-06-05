336d767c7e464e18a62dceb366a7df62513d89de
Changwoo Lim <limcw@zarathu.com>
2024-06-05 00:26:55 +0000

    Create team button is disabled if user is member of team

--- src/app/setting/team/CreateTeamModal.tsx
+++ src/app/setting/team/CreateTeamModal.tsx
@@ -9,19 +9,22 @@ import { createClient } from "@/utils/supabase/client";
 import { useVariable } from "@/hooks/useVariable";
 import { useToast } from "@/components/ui/use-toast";
 import { Spinner } from "@/components/global/Spinner";
-import { createTeam } from "@/hooks/fetch/useTeam";
+import { createTeam, useTeamInfo } from "@/hooks/fetch/useTeam";
 import { ActionButton } from "@/components/ui/actionbutton";
+import useSWRImmutable from "swr/immutable";
 export function CreateTeamModal() {
 	const { toast } = useToast();
 	const [openModal, setOpenModal] = useState<boolean>(false);
 	const [teamName, setTeamName, handleTeamName] = useVariable<string>("");
 	const [orgName, setOrgName, handleOrgName] = useVariable<string>("");
-
+	const { hasTeam, mutate } = useTeamInfo();
 	return (
 		<>
 			<Dialog open={openModal} onOpenChange={setOpenModal}>
 				<DialogTrigger asChild>
-					<Button variant="outline">팀 생성</Button>
+					<Button variant="outline" disabled={hasTeam}>
+						팀 생성
+					</Button>
 				</DialogTrigger>
 				<DialogContent className="sm:max-w-[425px]">
 					<DialogHeader>
@@ -54,6 +57,8 @@ export function CreateTeamModal() {
 										title: "팀 생성 성공",
 										description: `팀 ${teamName}이 생성되었습니다.`,
 									});
+
+									mutate();
 								} catch (e: any) {
 									toast({
 										title: "팀 생성 실패",
