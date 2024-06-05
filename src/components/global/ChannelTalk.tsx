e71307fe7ddd505b3dd5eaaf8025707c1b55dcf3
Changwoo Lim <limcw@zarathu.com>
2024-06-05 00:26:23 +0000

    Use both customer name and email as ChannelTalk name

--- src/components/global/ChannelTalk.tsx
+++ src/components/global/ChannelTalk.tsx
@@ -4,25 +4,33 @@ import * as ChannelService from "@channel.io/channel-web-sdk-loader";
 import { useEffect } from "react";
 ChannelService.loadScript();
 
+import useUser from "@/hooks/fetch/useUser";
+
 export default function ChannelTalk() {
+	const { userInfo, error, isLoading } = useUser();
+
 	useEffect(() => {
 		const supabase = createClient();
+
 		const getCurrentUserKey = async () => {
 			const {
 				data: { user },
 			} = await supabase.auth.getUser();
 			ChannelService.boot({
-				pluginKey: process.env.NEXT_PUBLIC_CHANNEL_TALK_PLUGIN_KEY as string, // fill your plugin key
+				pluginKey: process.env.NEXT_PUBLIC_CHANNEL_TALK_PLUGIN_KEY as string, // 채널톡 플러그인 키
 				memberId: user?.id,
 				profile: {
-					name: user?.email || null, // 의도적으로 email로 넣었습니다 => 추후 수정 필요; 채널톡 유료플랜 안 쓰면 이메일 못 보는데, 이름만 들어갈 경우에 회원 식별이 불가합니다.
+					name: userInfo?.last_name || userInfo?.first_name ? userInfo?.last_name + userInfo?.first_name + " " + user?.email : user?.email || null,
 					email: user?.email || null,
 					mobileNumber: user?.phone || null,
 				},
 			});
 		};
-		getCurrentUserKey();
-	}, []);
+
+		{
+			!isLoading && getCurrentUserKey();
+		}
+	}, [isLoading]);
 
 	return null;
 }
