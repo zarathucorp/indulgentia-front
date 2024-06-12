1de737e3740b37eb33fa667d47ae2e56c4c32cfe
Changwoo Lim <limcw@zarathu.com>
2024-06-12 08:00:07 +0000

    Breadcrumb 로딩 시 홈 아이콘 보이게 수정

--- src/components/global/Loading/BreadCrumb.tsx
+++ src/components/global/Loading/BreadCrumb.tsx
@@ -1,10 +1,15 @@
 import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbSeparator } from "@/components/ui/breadcrumb";
 import { Skeleton } from "@/components/ui/skeleton";
+import { FaHome } from "react-icons/fa";
 type DashboardBreadcrumbLoadingType = "Project" | "Bucket" | "Note";
 export function DashboardBreadCrumbLoading({ type }: { type: DashboardBreadcrumbLoadingType }) {
 	return (
 		<Breadcrumb>
 			<BreadcrumbList>
+				<BreadcrumbLink href="/">
+					<FaHome />
+				</BreadcrumbLink>
+				<BreadcrumbSeparator />
 				<BreadcrumbItem>
 					<Skeleton className="h-6 w-20" />
 				</BreadcrumbItem>
