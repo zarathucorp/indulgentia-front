aab04e655c859e486531dd88c3aeabebc335a853
Changwoo Lim <limcw@zarathu.com>
2024-06-13 09:02:33 +0000

    회원가입 진행 및 로그인 에러시 Toast

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,6 +1,7 @@
-import { headers } from "next/headers";
-import { createClient } from "@/utils/supabase/server";
-import { redirect } from "next/navigation";
+"use client";
+
+import { createClient } from "@/utils/supabase/client";
+
 import SubmitButton from "./submit-button";
 import GoogleLoginButton from "./google-login";
 import KakaoLoginButton from "./kakao-login";
@@ -14,52 +15,37 @@ import { Separator } from "@/components/ui/separator";
 import { Spinner } from "@/components/global/Spinner";
 import { FaGoogle } from "react-icons/fa";
 import { SendPasswordResetMailModal } from "./SendPasswordResetMailModal";
+import { useEffect } from "react";
+import signIn from "./signin";
+import { useToast } from "@/components/ui/use-toast";
 export default function SigninPage({ searchParams }: { searchParams: { message: string } }) {
-	const signIn = async (formData: FormData) => {
-		"use server";
+	const { toast } = useToast();
+	useEffect(() => {
+		setTimeout(() => {
+			console.log(searchParams.message);
+			if (searchParams.message === "Check email to continue sign in process") {
+				toast({
+					title: "이메일을 인증해주세요",
+					description: "메일로 전송된 링크를 클릭하여 이메일을 인증해주세요. 반드시 이 브라우저에서 링크를 클릭해주세요.",
+				});
+			} else if (searchParams.message === "Could not authenticate user") {
+				toast({
+					title: "로그인에 실패하였습니다.",
+					description: "이메일과 비밀번호를 정확히 확인해주세요",
+				});
+			} else {
+				toast({
+					title: "알 수 없는 상황입니다.",
+					description: decodeURI(searchParams.message),
+				});
+			}
+		}, 0);
+	}, [searchParams.message]);
 
-		const email = formData.get("email") as string;
-		const password = formData.get("password") as string;
-		const supabase = createClient();
-
-		const { error } = await supabase.auth.signInWithPassword({
-			email,
-			password,
-		});
-
-		if (error) {
-			return redirect("/auth/login?message=Could not authenticate user");
-		}
-
-		return redirect("/dashboard");
-	};
-
-	const signUp = async (formData: FormData) => {
-		"use server";
-
-		const origin = headers().get("origin");
-		const email = formData.get("email") as string;
-		const password = formData.get("password") as string;
-
-		const supabase = createClient();
-		const { error } = await supabase.auth.signUp({
-			email,
-			password,
-			options: {
-				emailRedirectTo: `${origin}/auth/callback`,
-			},
-		});
-
-		if (error) {
-			console.log(error);
-			return redirect("/auth/login?message=Could not authenticate user");
-		}
-
-		return redirect("/auth/login?message=Check email to continue sign in process");
-	};
 	return (
 		<>
 			<div className="pt-10">
+				{/* <ShowToast message={searchParams.message} /> */}
 				<Card className="mx-auto max-w-sm">
 					<CardHeader>
 						<CardTitle className="text-2xl">로그인</CardTitle>
@@ -93,7 +79,7 @@ export default function SigninPage({ searchParams }: { searchParams: { message: 
 							<Separator />
 							<CardDescription>외부 계정으로 로그인하기</CardDescription>
 							<GoogleLoginButton />
-							<KakaoLoginButton />
+							{/* <KakaoLoginButton /> */}
 						</div>
 						<div className="mt-4 text-center text-sm">
 							계졍이 없으신가요?{" "}
