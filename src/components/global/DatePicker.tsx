6abc29102d1b1bfba759ec8632d5e6ffc0e41581
Jaewoong Heo <koolerjaebee@gmail.com>
2024-06-13 15:09:20 +0900

    fix: change datepicker local to Korean

--- src/components/global/DatePicker.tsx
+++ src/components/global/DatePicker.tsx
@@ -3,6 +3,7 @@
 import * as React from "react";
 import { format } from "date-fns";
 import { Calendar as CalendarIcon } from "lucide-react";
+import { ko } from "date-fns/locale";
 import { FormControl } from "@/components/ui/form";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
@@ -15,13 +16,13 @@ export default function DatePicker(props: { field: any }) {
 			<PopoverTrigger asChild>
 				<FormControl>
 					<Button variant={"outline"} className={cn("w-[240px] pl-3 text-left font-normal", !props.field.value && "text-muted-foreground")}>
-						{props.field.value ? format(props.field.value, "PPP") : <span>날짜를 선택하세요.</span>}
+						{props.field.value ? format(props.field.value, "PPP", {locale: ko}) : <span>날짜를 선택하세요.</span>}
 						<CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
 					</Button>
 				</FormControl>
 			</PopoverTrigger>
 			<PopoverContent className="w-auto p-0" align="start">
-				<Calendar mode="single" selected={props.field.value} onSelect={props.field.onChange} initialFocus />
+				<Calendar mode="single" selected={props.field.value} onSelect={props.field.onChange} initialFocus locale={ko} />
 				{/* disabled={(date) => date > new Date() || date < new Date("1900-01-01")} */}
 			</PopoverContent>
 		</Popover>
