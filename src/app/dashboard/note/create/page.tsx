431d262960769b55b39f68d1055c7af4b222cfa3
Jaewoong Heo <koolerjaebee@gmail.com>
2024-11-04 16:48:29 +0900

    fix: move create breadcrumb to new component script

--- src/app/dashboard/note/create/page.tsx
+++ src/app/dashboard/note/create/page.tsx
@@ -1,37 +1,9 @@
-"use client";
-import useSWR from "swr";
-import axios from "axios";
-import { useSearchParams } from 'next/navigation'
-import { Label } from "@/components/ui/label";
-import { Input } from "@/components/ui/input";
-import { Textarea } from "@/components/ui/textarea";
-import { Button } from "@/components/ui/button";
 import NewNoteForm from "@/components/modules/dashboard/note/NoteForm";
-import { DashboardBreadCrumb } from "@/components/modules/dashboard/DashboardBreadCrumb";
-import { DashboardBreadCrumbLoading } from "@/components/global/Loading/BreadCrumb";
-
+import NoteCreateBreadCrumb from "@/components/modules/dashboard/note/NoteCreateBreadCrumb";
 export default function CreateNote() {
-  const searchParams = useSearchParams();
-	const bucket_uuid = searchParams.get("bucket");
-	const { data: breadcrumbData, isLoading: isBreadcrumbLoading } = useSWR(process.env.NEXT_PUBLIC_API_URL + `/dashboard/bucket/${bucket_uuid}/breadcrumb`, async (url) => {
-		const result = await axios.get(url);
-		if (result.status !== 200) {
-			const error = new Error("An error occurred while fetching the data.");
-			throw error;
-		}
-		console.log(result.data.data);
-		return result.data.data;
-	});
-
 	return (
 		<>
-			<div className="py-3 pl-4">
-				{isBreadcrumbLoading ? (
-					<DashboardBreadCrumbLoading type="Note" />
-				) : (
-				<DashboardBreadCrumb breadcrumbData={{ level: "Note", ...breadcrumbData, bucket_id: bucket_uuid, note_title: "[새 노트]", note_id: "" }} />
-				)}
-			</div>
+			<NoteCreateBreadCrumb />
 			<NewNoteForm />
 		</>
 	);
---
+++ src/components/modules/dashboard/bucket/BucketCreateBreadCrumb.tsx
@@ -0,0 +1,29 @@
+"use client";
+import useSWR from "swr";
+import axios from "axios";
+import { useSearchParams } from 'next/navigation'
+import { DashboardBreadCrumb } from "@/components/modules/dashboard/DashboardBreadCrumb";
+import { DashboardBreadCrumbLoading } from "@/components/global/Loading/BreadCrumb";
+export default function BucketCreateBreadCrumb() {
+  const searchParams = useSearchParams();
+	const project_uuid = searchParams.get("project");
+	const { data: breadcrumbData, isLoading: isBreadcrumbLoading } = useSWR(process.env.NEXT_PUBLIC_API_URL + `/dashboard/project/${project_uuid}`, async (url) => {
+		const result = await axios.get(url);
+		if (result.status !== 200) {
+			const error = new Error("An error occurred while fetching the data.");
+			throw error;
+		}
+		console.log(result.data.data);
+		return result.data.data;
+	});
+
+	return (
+    <div className="py-3 pl-4">
+      {isBreadcrumbLoading ? (
+        <DashboardBreadCrumbLoading type="Bucket" />
+      ) : (
+        breadcrumbData && <DashboardBreadCrumb breadcrumbData={{ level: "Bucket", project_id: breadcrumbData.id, project_title: breadcrumbData.title, bucket_title: "[새 버킷]", bucket_id: "" }} />
+      )}
+    </div>
+	);
+}
