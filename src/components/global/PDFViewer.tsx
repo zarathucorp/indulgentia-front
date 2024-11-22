9827bdb142c5aa608178a83c4641765b94c445b5
Jaewoong Heo <koolerjaebee@gmail.com>
2024-11-22 18:08:11 +0900

    fix: remove download/upload button on pdf viewer

--- src/components/global/PDFViewer.tsx
+++ src/components/global/PDFViewer.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { Worker, Viewer } from "@react-pdf-viewer/core";
 import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
+import { toolbarPlugin, ToolbarSlot, TransformToolbarSlot, ToolbarPlugin } from '@react-pdf-viewer/toolbar';
 import "@react-pdf-viewer/core/lib/styles/index.css";
 import "@react-pdf-viewer/default-layout/lib/styles/index.css";
 
@@ -10,8 +11,18 @@ interface PDFViewerProps {
 }
 
 const PDFViewer: React.FC<PDFViewerProps> = ({ fileUrl, className }) => {
-	const defaultLayoutPluginInstance = defaultLayoutPlugin();
+	const toolbarPluginInstance = toolbarPlugin();
+	const { renderDefaultToolbar, Toolbar } = toolbarPluginInstance;
 
+	const transform: TransformToolbarSlot = (slot: ToolbarSlot) => ({
+		...slot,
+		Download: () => <></>,
+		Open: () => <></>,
+	});
+	const renderToolbar = (ToolbarComponent: typeof Toolbar) => <ToolbarComponent>{renderDefaultToolbar(transform)}</ToolbarComponent>;
+	const defaultLayoutPluginInstance = defaultLayoutPlugin({
+		renderToolbar,
+	});
 	return (
 		<div className={className}>
 			<Worker workerUrl={`https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js`}>
